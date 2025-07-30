e46896751813f6c517c582c9e91c27a743bf6811
herbgrace <thelen.alexander05@gmail.com>
2025-07-29 20:15:54 -0600

    Added Random Quotes to index.html

--- scripts/quoteAPI.js
+++ scripts/quoteAPI.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
+    fetchQuote();
+
+    async function fetchQuote() {
+        try{
+            const API_KEY = "PUT API KEY HERE"
+            const LINK = "https://api.api-ninjas.com/v1/quotes?X-Api-Key=" + API_KEY;
+            const response = await fetch(LINK);
+
+            if (!response.ok) {
+                throw new Error("Unable to fetch the quote of the day, please try again later.")
+            }
+            const quote = await response.json();
+            console.debug(quote);
+            document.getElementById("quote-text").textContent = "\"" + quote[0].quote + "\"";
+            document.getElementById("author").textContent = "-" + quote[0].author;
+        } catch (err) {
+            console.debug(err);
+        }
+    }
 })
